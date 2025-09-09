b2d0b0e0b00a17f9efebc5d67471c2749318a6b7
ronBP95 <ronmagpantay9513@gmail.com>
2025-09-08 22:43:44 -0700

    Add Login skeleton

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,5 +1,16 @@
 export default function Page() {
     return (
-        <p>Login Page</p>
+        <div>
+            <h1>Logo goes here</h1>
+            <div>
+                <p>Login</p>
+                <p>Don't have an account yet? Sign up now!</p>
+                <input type="text" placeholder="Email" />
+                <input type="password" placeholder="Password" />
+                <p>Forgot your password?</p>
+                <p>Show Password</p>
+                <p>Login</p>
+            </div>
+        </div>
     );
 }
